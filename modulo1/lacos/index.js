14963d8a3620117fde5bccf17c715bb84d8cd238
rafix923 <rafaellopes.dev@gmail.com>
2022-05-03 21:59:19 -0300

    Exercício lacos em javascript

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,45 @@
+//---- Exercício de interpretação de código 1:
+
+/* 1. Está incrementando em 1 cada vez que compara se i é menor que
+5. E a cada rodada esse valor é acrescentado à propriedade 'valor'. */
+
+//---- Exercício de interpretação de código 2:
+
+// a) Serão impressos os valores: 19,21,23,25,27,30;
+
+// b) Apenas for...Off.. não seria suficiente.
+//Poderia ser usado o método entries() para acessar o índice de cada elemento
+
+//---- Exercício de interpretação de código 3:
+
+/* Será impresso 4 linhas com acrescentando um asterísrico em cada linha até o total
+de quatro asterísticos na quarta linha */
+
+
+//---- Exercício de esccrita de código 1:
+
+let quantBichinhos = Number(prompt("Quantos bichinhos de estimação você tem?"));
+
+if (quantBichinhos === 0) {
+    console.log("Que pena! Você pode adotar um pet!")
+} else(quantBichinhos > 0) {
+    let nomeDosBichinhos = []
+    for (let i = 0; i < quantBichinhos);
+}
+
+//---- Exercício de esccrita de código 2:
+
+// Resposta item a:
+
+const arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+for (let valor of arrayOriginal) {
+    console.log(valor)
+}
+
+// Resposta item b:
+
+const arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+for (let valor of arrayOriginal) {
+    let valor = Number(arrayOriginal / 10)
+    console.log(valor)
+}
