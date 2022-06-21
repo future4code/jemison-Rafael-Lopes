69c2a67161ff43db66df73d2addea86f1dc53668
rafix923 <rafaellopes.dev@gmail.com>
2022-06-21 11:05:38 -0300

    Estilizar dom

--- modulo-3/intro-react/primeiro-react/src/App.js
+++ modulo-3/intro-react/primeiro-react/src/App.js
@@ -1,19 +1,23 @@
-import logoLAbenu from './img/logo.png';
+import React from 'react';
+import logoLabenu from './img/logo.png';
 import './App.css';
 
 function App() {
-const mandarMensagem = () =>{
-  alert('Vamos desenvolver juntos!')
-}
-   return (
-    <div className="App">
-     <h1>Olá! Eu sou a Labenu!</h1>
-        <img src={logoLAbenu} className="App-logo" alt="logo" />
-        <p>
-          Este é o meu primeiro site React.
-        </p>
-        <a href='https://www.labenu.com.br/'> Site da Labenu !</a>
+  const mandarMensagem = () => {
+    alert('Vamos desenvolver juntos!')
+  }
+  return (
+    <div className="body">
+      <h1>Olá! Eu sou a Labenu!</h1>
+      <div className = 'logo'>
+        <img src={logoLabenu} alt="logo" />
+      </div>
+      <p>
+        Este é o meu primeiro site React.
+      </p>
+      <div>
       <button onClick={mandarMensagem}>Aperte este botão</button>
+      </div>
     </div>
   );
 }
