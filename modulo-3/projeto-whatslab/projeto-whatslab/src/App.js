07e72998e875b8af5f9d4cdbb5392bad38d00dc6
rafix923 <rafaellopes.dev@gmail.com>
2022-07-08 11:15:23 -0300

    Criar componente Messenger e exportar para App.js

--- modulo-3/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo-3/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
-
+import Messenger from "./components/Messenger/Messenger";
 function App() {
   return (
     <div>
-     
+     <Messenger />
     </div>
   );
 }
---
+++ modulo-3/projeto-whatslab/projeto-whatslab/src/components/Messenger/Messenger.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+Menssenger = () =>{
+ <div>
+
+ </div>   
+}
+
+export default Messenger;
