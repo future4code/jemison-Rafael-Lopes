480166c71b64f0ebac5781735a00ede2612cc9b8
rafix923 <rafaellopes.dev@gmail.com>
2022-07-12 21:33:21 -0300

    Editar informações da segunda e terceira etapa

--- modulo-3/rend-condicional-form/labenu-forms/src/components/PrimeiraEtapa.js
+++ modulo-3/rend-condicional-form/labenu-forms/src/components/PrimeiraEtapa.js
@@ -2,31 +2,30 @@ import React from 'react'
 
 function Etapa1() {
   return (
-    
-          <fieldset>
+
+    <fieldset>
       <h1>Dados Gerais</h1>
       <p>Preencha os dados solicitados abaixo.</p>
-        <form>
-            <label for='nome'>1. Qual o seu nome?</label>
-            <input name='nome' type='text' required />
-            <label for='idade'>2. Qual sua idade?</label>
-            <input name='idade' type='text' min='18' required></input>
-            <label for='email'>3. Qual seu email?</label>
-            <input name='email' type='email' />
-            <section>
-              <label for='escolaridade'>4. Qual a sua escolaridade?</label>
-              <select name='escolaridade'>
-                <option>Ensino fundamental completo</option>
-                <option>Ensino médio completo</option>
-                <option>Curso superior completo</option>
-                <option>Posgraduação</option>
-                <option>Mestrado</option>
-              </select>
-            </section>
+      <form>
+        <label for='nome'>1. Qual o seu nome?</label>
+        <input name='nome' type='text' required />
+        <label for='idade'>2. Qual sua idade?</label>
+        <input name='idade' type='text' min='18' required></input>
+        <label for='email'>3. Qual seu email?</label>
+        <input name='email' type='email' />
+        <section>
+          <label for='escolaridade'>4. Qual a sua escolaridade?</label>
+          <select name='escolaridade'>
+            <option>Ensino Médio Incompleto</option>
+            <option>Ensino Médio Completo</option>
+            <option>Curso superior Incompleto</option>
+            <option>Ensino Médio Completo</option>
+          </select>
+        </section>
 
-        </form>
-          </fieldset>
-    
+      </form>
+    </fieldset>
+
   )
 }
 
--- modulo-3/rend-condicional-form/labenu-forms/src/components/TerceiraEtapa.js
+++ modulo-3/rend-condicional-form/labenu-forms/src/components/TerceiraEtapa.js
@@ -5,9 +5,9 @@ function TerceiraEtapa() {
     <fieldset>
     <h1>Informações Gerais de Ensino</h1>
       <form>
-        <label form='informacoes-de-ensino'>5.1 Por que você não terminou um curso de graduação</label>
+        <label form='informacoes-de-ensino'>5. Por que você não terminou um curso de graduação</label>
         <input name='informacoes-de-ensino' type='text' required />
-        <label>6.1 Você fez algum curso complementar?</label>
+        <label>6. Você fez algum curso complementar?</label>
         <select>
           <option value=''>(Selecione Opção)</option>
           <option value='1'>sim</option>
