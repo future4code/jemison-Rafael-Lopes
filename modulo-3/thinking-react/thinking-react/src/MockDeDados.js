a95aaf7594ef167d76b89af5be967ef37a4f151a
rafix923 <rafaellopes.dev@gmail.com>
2022-07-14 05:19:49 -0300

    Criar mock de dados

---
+++ modulo-3/thinking-react/thinking-react/src/MockDeDados.js
@@ -0,0 +1,6 @@
+export const produtos = [
+    { id: 1, name: "Produto 1", valor: 130, imagem: "https://picsum.photos/200/200?a=1" },
+    { id: 2, name: "Produto 2", valor: 130, imagem: "https://picsum.photos/200/200?a=1" },
+    { id: 3, name: "Produto 3", valor: 130, imagem: "https://picsum.photos/200/200?a=1" },
+    { id: 4, name: "Produto 4", valor: 130, imagem: "https://picsum.photos/200/200?a=1" }
+]
