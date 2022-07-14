83439beb598a51f0951e726f745cbda9e63c4359
rafix923 <rafaellopes.dev@gmail.com>
2022-07-14 14:40:45 -0300

    Adicionar mais dois produtos ao mock de dados

--- modulo-3/thinking-react/thinking-react/src/MockDeDados.js
+++ modulo-3/thinking-react/thinking-react/src/MockDeDados.js
@@ -2,5 +2,7 @@ export const produtos = [
     { id: 1, name: "Produto 1", valor: 130, imagem: "https://picsum.photos/200/200?a=1" },
     { id: 2, name: "Produto 2", valor: 130, imagem: "https://picsum.photos/200/200?a=1" },
     { id: 3, name: "Produto 3", valor: 130, imagem: "https://picsum.photos/200/200?a=1" },
-    { id: 4, name: "Produto 4", valor: 130, imagem: "https://picsum.photos/200/200?a=1" }
+    { id: 4, name: "Produto 4", valor: 130, imagem: "https://picsum.photos/200/200?a=1" },
+    { id: 5, name: "Produto 5", valor: 130, imagem: "https://picsum.photos/200/200?a=1" },
+    { id: 6, name: "Produto 6", valor: 130, imagem: "https://picsum.photos/200/200?a=1" }
 ]
