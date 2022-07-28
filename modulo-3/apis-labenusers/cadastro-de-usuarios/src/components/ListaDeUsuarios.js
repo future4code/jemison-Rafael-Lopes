904ea39ab51285fc5ed2077b16179496dc6f7c71
rafix923 <rafaellopes.dev@gmail.com>
2022-07-28 10:52:15 -0300

    Criar componentes Cadastro e Lista de usuários

---
+++ modulo-3/apis-labenusers/cadastro-de-usuarios/src/components/ListaDeUsuarios.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListaDeUsuarios() {
+  return (
+    <div>
+        <h1>Lista de Usuários</h1>
+    </div>
+  )
+}
+
+export default ListaDeUsuarios
---
+++ modulo-3/apis-labenusers/cadastro-de-usuarios/src/components/Seletor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Seletor() {
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default Seletor
