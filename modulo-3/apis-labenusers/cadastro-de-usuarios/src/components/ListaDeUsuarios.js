a0c2e67851d07389ae5797dc04abf77a28f99cd3
rafix923 <rafaellopes.dev@gmail.com>
2022-07-28 18:09:23 -0300

    Aplicar lifecicle para busca de dados

--- modulo-3/apis-labenusers/cadastro-de-usuarios/src/components/ListaDeUsuarios.js
+++ modulo-3/apis-labenusers/cadastro-de-usuarios/src/components/ListaDeUsuarios.js
@@ -1,11 +1,31 @@
 import axios from 'axios';
 import React from 'react'
+import { useEffect } from 'react';
 
 function ListaDeUsuarios(props) {
+
+    // Variáveis da API
     const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users';
-    getAllUsers = ()=>{
-        axios.get(url)
+    const serviceHeaders = {
+        headers: {
+            Authorization: 'rafael-lopes-jemison'
+        }
+    };
+    // Requisição GET
+    getAllUsers = () => {
+        axios.get(url, serviceHeaders)
+            .then((response) => {
+                alert('Usuário cadastrado com sucesso!')
+                getAllUsers()
+            }).catch((error) => {
+                alert('Algo errado. Tente novamente!')
+            })
     }
+
+    // Buscando dados automaticamente utilizando o método de ciclo de vida(lifecycle)
+    useEffect(() => {
+        getAllUsers()
+    }, [])
     const { handleCadastro } = props;
 
     return (
