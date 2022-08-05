a074f2b61bd8b2819ebe4ff02074c7b7d2dc2fe6
rafix923 <rafaellopes.dev@gmail.com>
2022-08-05 09:38:39 -0300

    Adicionar lógica no app.js

--- modulo3/projeto-astromatch/astromatch-app/src/App.js
+++ modulo3/projeto-astromatch/astromatch-app/src/App.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import Main from "./components/Main/Main";
+import MatchPage from "./components/MatchPage/MatchPage";
 
 function App() {
+
+  const [screen, setScreen] = useState("mainScreen")
+
+  const mainScreen= (screen)=> {
+   setScreen(screen)
+  }
+
+  const switchScreen = ()=>{
+    switch(screen){
+      case "mainScreen":
+        return<Main mainScreen={mainScreen} />
+        case "matchList":
+          return <MatchPage mainScreen={mainScreen} />
+          default:
+            return null;
+    }
+  } 
+
   return (
     <div>
-      <Main />
+    {switchScreen()}
     </div>
   );
 }
