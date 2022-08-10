e14df21e8de700c6fcce626ea817970efe0827dd
rafix923 <rafaellopes.dev@gmail.com>
2022-08-10 17:35:31 -0300

    Criar esqueleto das páginas

---
+++ modulo4/projeto-labex/labex/src/components/Pages/AdminHomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminHomePage() {
+  return (
+    <div>
+    <h1>AdminHomePage</h1>
+    </div>
+  )
+}
+
+export default AdminHomePage;
---
+++ modulo4/projeto-labex/labex/src/components/Pages/ListTripsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListTripsPage() {
+  return (
+    <div>
+    <h1>ListTripsPage</h1>
+    </div>
+  )
+}
+
+export default ListTripsPage;
