44b4a3bdb95adb60bcea81adc41fcb89dc85c00d
rafix923 <rafaellopes.dev@gmail.com>
2022-08-15 15:08:12 -0300

    Criar uma BASE_URL

--- modulo4/projeto-labex/labex/src/App.js
+++ modulo4/projeto-labex/labex/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Router from './components/Router';
-import UseRequestData from './components/Hooks/UseRequestData';
+// import useRequestData from './components/Hooks/UseRequestData';
 
 function App() {
 
-  const xxxxx = UseRequestData(`${BASE_URL}parametro`)
+  // const [xxx, isLoadingXXX, errorXXX] = useRequestData(`${BASE_URL}`parametro)
+  // const [yyy, isLoadingYYY, errorYYY] = useRequestData(`${BASE_URL}`outroParametro)
+
+  // const listaDeX = xxx && xxx.map((x) => {
+  //   return <p>{x.name}</p>
+  // })
+
+  // const listaDeY = yyy && yyy.map((x) => {
+  //   return <p>{y.name}</p>
+  // })
+
+
   return (
     <div className="App">
-      <Router/>
+      {/* {isLoadingXXX && <p>Carregando X</p>}
+      {isLoadingYYY && <p>Carregando Y</p>}
+      {!isLoadingXXX && errorXXX && <p>Algo deu errado com o X!</p>}
+      {!isLoadingYYY && errorYYY && <p>Algo deu errado com o Y!</p>} */}
+      <Router />
     </div>
   );
 }
--- modulo4/projeto-labex/labex/src/components/Hooks/UseRequestData.js
+++ modulo4/projeto-labex/labex/src/components/Hooks/UseRequestData.js
@@ -1,21 +1,28 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function UseRequestData(url) {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState(undefined)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState("")
 
-    useEffect(()=>{
-        axios.get(url)
-        .then((response)=>{
-            setData(response.data)
-    }).catch((error)=>{
-        console.log(error)
-    })}, [url])
+    useEffect(() => {
+        setIsLoading(true);
+        axios
+            .get(url)
+            .then((response) => {
+                setIsLoading(false)
+                setData(response.data)
+            }).catch((error) => {
+                setIsLoading(false)
+                console.log(error)
+                setError(error)
+            })
+    }, [url])
 
-  return (
-    data
-  )
+    return [data, isLoading, error]
+
 }
 
-export default UseRequestData
+export default UseRequestData;
