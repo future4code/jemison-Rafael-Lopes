960feff2dfe75301e7461a213d7b73ff98e08c85
rafix923 <rafaellopes.dev@gmail.com>
2022-08-15 10:47:56 -0300

    Cria código para o useRequestData e importar para app.js

--- modulo4/projeto-labex/labex/src/App.js
+++ modulo4/projeto-labex/labex/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Router from './components/Router';
+import UseRequestData from './components/Hooks/UseRequestData';
 
 function App() {
+
+  const xxxxx = UseRequestData(`${BASE_URL}parametro`)
   return (
     <div className="App">
       <Router/>
