13967177eeb338f111b9c14233c7c8947b012573
rafix923 <rafaellopes.dev@gmail.com>
2022-08-16 15:51:25 -0300

    Estilizar e chamar o body no app.js

--- modulo3/projeto-styled-components/styled-components-version2/src/App.js
+++ modulo3/projeto-styled-components/styled-components-version2/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Body from './components/Body/Body';
 import Header from './components/Header/Header';
 import { Imagem } from './components/Header/style';
 import logoLabenu from './logo/logoLabenu.jpg'
@@ -10,6 +11,7 @@ function App() {
     <img src={logoLabenu} alt="A Logo da Labenu" />
   </Imagem>
   <Header/>
+  <Body/>
     </div>
   );
 }
