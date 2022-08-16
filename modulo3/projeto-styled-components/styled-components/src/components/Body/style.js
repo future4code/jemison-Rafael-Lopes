e5ec396f6e4e52491957147c85a7e307b632d6c6
rafix923 <rafaellopes.dev@gmail.com>
2022-08-16 14:04:57 -0300

    Criar Body e estilizar

---
+++ modulo3/projeto-styled-components/styled-components/src/components/Body/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const DivPai=styled.div`
+background-color:lightgreen;
+height: 90vh;
+display: flex;
+align-items: center;
+justify-content:space-between;
+`
+
+export const Colunas=styled.span`
+display:flex;
+background-color: orange;
+height: 90vh;
+width: 10vw;
+`
+export const Input=styled.section`
+display: flex;
+font-size: 1.2rem;
+margin-top:55vh;
+justify-content: left;
+`
