92932f24545701be1501af76c509eac36592142a
rafix923 <rafaellopes.dev@gmail.com>
2022-08-16 14:01:31 -0300

    Criar componentes Header, body e footer

---
+++ modulo3/projeto-styled-components/styled-components/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ContainerPai, TituloPrincipal } from './style'
+
+
+function Header() {
+    return (
+
+        <ContainerPai>
+            <TituloPrincipal>LabZap/ WhatsLab</TituloPrincipal>
+        </ContainerPai>
+    );
+}
+
+export default Header;
