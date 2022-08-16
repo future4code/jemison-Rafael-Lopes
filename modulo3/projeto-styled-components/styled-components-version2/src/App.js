8a1ab4463a94c87f424d3d99f300740f760ab0c0
rafix923 <rafaellopes.dev@gmail.com>
2022-08-16 15:53:46 -0300

    Estilizar e chamar o footer no app.js

--- modulo3/projeto-styled-components/styled-components-version2/src/App.js
+++ modulo3/projeto-styled-components/styled-components-version2/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Body from './components/Body/Body';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import { Imagem } from './components/Header/style';
 import logoLabenu from './logo/logoLabenu.jpg'
@@ -7,11 +8,12 @@ import logoLabenu from './logo/logoLabenu.jpg'
 function App() {
   return (
     <div className="ContainerPai">
-  <Imagem>
-    <img src={logoLabenu} alt="A Logo da Labenu" />
-  </Imagem>
-  <Header/>
-  <Body/>
+      <Imagem>
+        <img src={logoLabenu} alt="A Logo da Labenu" />
+      </Imagem>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
