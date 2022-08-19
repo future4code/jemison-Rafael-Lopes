3473fc506ba2056b49eb2e1db17aef6964159d5e
rafix923 <rafaellopes.dev@gmail.com>
2022-08-19 07:56:58 -0300

    Primeiro código listTripPage

--- modulo4/projeto-labex/labex/src/components/Pages/ListTripsPage.js
+++ modulo4/projeto-labex/labex/src/components/Pages/ListTripsPage.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-
 
 
 function ListTripsPage() {
 
+  const [trips, setTrips] = useState(null)
 
+  const listTrips = trips.map((trip)=>{
+    return(
+      <section>
+  <h2>{trip.name}, {trip.planet}</h2>
+      <p>Duração: {trip.durationInDays}</p>
+      <p>Descrição: {trip.description}</p>
+      </section>
+    
+    )
+  })
+
+  
   const navigate = useNavigate();
 
   const goToHome = () => {
@@ -24,7 +36,7 @@ function ListTripsPage() {
       <h2>Lista de viagens</h2>
       <button onClick={goToHome} >Voltar</button>
       <button onClick={goToAplicationFormPage} >Inscrever-se</button>
-     
+   {listTrips}
     </div>
   )
 }
