eecc41a6e9353206e2ebd3d6eb3c6ff46c40397c
rafix923 <rafaellopes.dev@gmail.com>
2022-09-24 10:23:18 -0300

    codigo para etapa2

---
+++ modulo3/renderizao-condicional-v2.0/labenu-forms/src/components/Etapa2.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+
+export function Etapa2 ({ mudaPagina }) {
+
+    const [curso, setCurso] = useState("")
+    const [unidadeDeEnsino, setUnidadeDeEnsino] = useState("")
+
+
+    function handleCurso (event) {
+        setCurso(event.target.value)
+    }
+
+    function handleUnidadeDeEnsino (event) {
+        setUnidadeDeEnsino(event.target.value)
+    }
+
+    function handleSubmit (event) {
+        event.preventDefault()
+        mudaPagina("4")
+    }
+
+    return(
+        <div>
+             <h1>Etapa 2</h1>
+
+             <form onSubmit={handleSubmit}>
+                <label>Qual o curso?   
+                  <input type="text" value={curso} onChange={handleCurso}/>  
+                </label>
+                
+                <label>Qual unidade de ensino?  
+                    <input type="text" value={unidadeDeEnsino} onChange={handleUnidadeDeEnsino}/>
+                </label>
+                
+                <input type="submit" value="Enviar" />
+                
+             </form>
+             
+        </div>
+       
+    )
+}
