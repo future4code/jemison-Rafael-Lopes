bd488cdabc5ce92cd74672cafe045867dfd66a0e
rafix923 <rafaellopes.dev@gmail.com>
2022-09-24 10:26:39 -0300

    codigo para etapa final

---
+++ modulo3/renderizao-condicional-v2.0/labenu-forms/src/components/Final.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export function EtapaFinal () {
+    return(
+        <div>
+            <h1>Etapa Final</h1>
+
+            <h2>Obrigado por responder o formulário. Entraremos em contato em breve.</h2>
+             
+        </div>
+       
+    )
+}
