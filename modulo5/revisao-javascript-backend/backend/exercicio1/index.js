829f7c76a353ea0544eb7a682915fc96ee2a4cd8
rafix923 <rafaellopes.dev@gmail.com>
2022-10-04 22:12:49 -0300

    Se adicionar esse commit, irá criar a função do exercicio 1 para converter temperaturas

--- modulo5/revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,1 +1,7 @@
-console.log("exercicio 1");
+const ConversorDeTemperatura = (temperatura) => {
+    const tCelsius = temperatura;
+    const celsiusParaFahrenheit = tCelsius * 9 / 5 + 32;
+    let texto = tCelsius + '\xB0C é ' + celsiusParaFahrenheit + ' \xB0F';
+    console.log(ConversorDeTemperatura(20, texto))
+}
+
