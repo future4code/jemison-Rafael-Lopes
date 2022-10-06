09194c854c2999a041b860c93b25ce248a9c0e0b
rafix923 <rafaellopes.dev@gmail.com>
2022-10-06 16:52:04 -0300

    Se adicionar esse commit, incluirá a cliente maria no array da lista de cliente

--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,1 +1,20 @@
-console.log("exercício 2");
+let listaDeClientes = [
+    {
+        id: 1,
+        nome: "Fulano"
+    },
+    {
+        id: 2,
+        nome: "Ciclano"
+    },
+    {
+        id: 3,
+        nome: "Beltrano"
+    },
+    {
+        id: 4,
+        nome: "Fulana"
+    }
+]
+listaDeClientes.push({id: 5, nome:"Maria"}),
+console.log(listaDeClientes);
