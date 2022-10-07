12692845756144be0aee1fee2fb8f4ec4cfb192d
rafix923 <rafaellopes.dev@gmail.com>
2022-10-07 18:27:44 -0300

    Se adicionar esse commit, irá acrescentar cósigo de teste ao exercício4

--- modulo5/revisao-javascript-backend/backend/exercicio3/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio3/index.js
@@ -1,1 +1,12 @@
-console.log("exercicio 3");
+const contas = [
+	{
+		email: "astrodev@labenu.com",
+		password: "abc123"
+	},
+	{
+		email: "bananinha@gmail.com",
+		password: "bananinha"
+	}
+]
+console.log(contas);
+
--- modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,1 +1,18 @@
-console.log("exercicio 4");
+const primeiraLista = [
+	{
+		nome: "Banana"
+	},
+	{
+		nome: "Laranja"
+	}
+]
+
+const segundaLista= [
+	{
+		nome: "Laranja"
+	},
+	{
+		nome: "Cebola"
+	}
+]
+console.log(geraItensUnicos(primeiraLista, segundaLista))
