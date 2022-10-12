cee3c85514059aa6b3315735167fb3dd0ae3c840
rafix923 <rafaellopes.dev@gmail.com>
2022-10-12 07:55:26 -0300

    Se adicionar esse commit, irá renomear o arquivo meusDados.js para index.js

---
+++ modulo5/node-package-json/exercicio1/index.js
@@ -0,0 +1,10 @@
+// a) Responda como comentário no seu código: como fazemos para acessar os parâmetros passados na linha de comando para o Node?
+// Resposta: Utilizando o process.argv
+
+// b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
+// "Olá, (Nome)! Você tem (sua idade) anos."
+
+const nome = "Rafael";
+const idade = 30;
+
+
--- modulo5/node-package-json/exercicio1/meusDados.js
+++
@@ -1,4 +0,0 @@
-// a) Responda como comentário no seu código: como fazemos para acessar os parâmetros passados na linha de comando para o Node?
-// Resposta: Utilizando o process.argv
-
-// b)
