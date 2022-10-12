900c0120146ea515e4e1038f2746d1315a69b26b
rafix923 <rafaellopes.dev@gmail.com>
2022-10-12 09:16:54 -0300

    Se adicionar esse commit, incluirá solução para o quesito 'b' do exercício 1

--- modulo5/node-package-json/exercicio1/index.js
+++ modulo5/node-package-json/exercicio1/index.js
@@ -4,7 +4,14 @@
 // b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
 // "Olá, (Nome)! Você tem (sua idade) anos."
 
-const nome = "Rafael";
-const idade = 30;
+const myName = "Rafael";
+const myAge = 30;
+const statement = `Olá, ${myName}! Você tem ${myAge} anos.`;
 
+function message() {
+    const result = statement;
+    return result
+}
+const result = message()
+console.log(result);
 
