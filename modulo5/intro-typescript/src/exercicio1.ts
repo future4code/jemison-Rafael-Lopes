fe952ee66f7ac49fca4468b33b4400fa9c242bfa
rafix923 <rafaellopes.dev@gmail.com>
2022-10-14 16:59:12 -0300

    Se adicionar esse commit, irá refatorar o exercício 2 para typescript.

--- modulo5/intro-typescript/src/exercicio1.ts
+++ modulo5/intro-typescript/src/exercicio1.ts
@@ -2,11 +2,10 @@
 // ladoA, ladoB, ladoC e retorna se ele é equilátero, isósceles ou escaleno.
 // Refatore a função para o Typescript.
 
-const ladoA: number = 5
-const ladoB: number = 5
-const ladoC: number = 15
+
 
 function checaTriangulo(a: number, b: number, c: number):string {
+    
     if (a !== b && b !== c) {
         return "Escaleno";
     } else if (a === b && b === c) {
