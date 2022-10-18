ea21730f7e862bfb752cafaffa9a6608948d54af
rafix923 <rafaellopes.dev@gmail.com>
2022-10-18 09:48:13 -0300

    Se adicionar esse commit, incluirá resposta para a parte 'a' do exercicio 1.

---
+++ modulo5/aprofundamento-typescript/src/exercicio1.ts
@@ -0,0 +1,7 @@
+// a) Crie uma variável minhaString do tipo string e atribua um valor a ela. Tente atribuir um número a esta variável. O que acontece?
+
+let minhaString:string = "Hello World!"
+// let minhaString:string = 9
+// Resposta: Se atribuir um número a uma variável que foi tipada como string, o TypeScript informará que:
+// 'O tipo número não é atribuível ao tipo string'.
+console.log(minhaString);
