9226df35bc5319d3c80ff58d889c9d483194d972
rafix923 <rafaellopes.dev@gmail.com>
2022-10-19 08:14:37 -0300

    Se adicionar esse commit, incluirá resposta para a parte 'a' do exercicio 2.

--- modulo5/aprofundamento-typescript/src/exercicio2.ts
+++ modulo5/aprofundamento-typescript/src/exercicio2.ts
@@ -1,27 +1,47 @@
 // Observe a função a seguir, escrita em JavaScript:
-// function obterEstatisticas(numeros) {
 
+// function obterEstatisticas(numeros) {
 //     const numerosOrdenados = numeros.sort(
 //         (a, b) => a - b
 //     )
-
 //     let soma = 0
-
 //     for (let num of numeros) {
 //         soma += num
 //     }
-
 //     const estatisticas = {
 //         maior: numerosOrdenados[numeros.length - 1],
 //         menor: numerosOrdenados[0],
 //         media: soma / numeros.length
 //     }
-
 //     return estatisticas
 // }
 
 // a) Quais são as entradas e saídas dessa função? Copie a função para um arquivo .ts e faça a tipagem desses parâmetros
+
+// Resposta: As entradas são números ordenados e a saída é a estatísitca dos números maiores, menores e a média deles.
+
+function obterEstatisticas(numeros:number[]) {
+
+    const numerosOrdenados:number[] = numeros.sort(
+        (a:number, b:number) => a - b
+    )
+
+    let soma:number = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+        return estatisticas
+}
+
 // b) Quais outras variáveis compõem essa função? Explicite a tipagem de todas elas
+
 // c) Crie um type chamado amostra de dados, isto é, um objeto com as propriedades numeros e obterEstatisticas.
 // Abaixo, temos um exemplo de objeto desse tipo, declarado em JavaScript:
 // const amostraDeIdades = {
