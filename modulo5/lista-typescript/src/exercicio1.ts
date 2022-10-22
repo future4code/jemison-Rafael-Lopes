de46fac9aded58f3d4d7b41968741a64579b4702
rafix923 <rafaellopes.dev@gmail.com>
2022-10-21 21:24:59 -0300

    Se adicionar esse commit, criará o arquivo exercicio1.ts e adicionará o enunciado.

---
+++ modulo5/lista-typescript/src/exercicio1.ts
@@ -0,0 +1,3 @@
+// Crie um função que receba uma string com o nome e outra string com uma data de nascimento (ex.: “24/04/1993”). 
+// A função deve separar o dia, o mês e ano e retornar uma string no seguinte formato: 
+// "Olá me chamo {NOME}, nasci no dia {DIA} do mês de {MÊS} do ano de {ANO}" 
