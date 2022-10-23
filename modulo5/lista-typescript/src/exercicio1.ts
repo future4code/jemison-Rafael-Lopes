2729fc7e0894b480121af63de3b1ab63d59537c5
rafix923 <rafaellopes.dev@gmail.com>
2022-10-23 10:19:32 -0300

    Se adicionar esse commit, adicionará resposta para o exercicio 1

--- modulo5/lista-typescript/src/exercicio1.ts
+++ modulo5/lista-typescript/src/exercicio1.ts
@@ -1,3 +1,14 @@
 // Crie um função que receba uma string com o nome e outra string com uma data de nascimento (ex.: “24/04/1993”). 
 // A função deve separar o dia, o mês e ano e retornar uma string no seguinte formato: 
 // "Olá me chamo {NOME}, nasci no dia {DIA} do mês de {MÊS} do ano de {ANO}" 
+
+function person(name: string, birth: string):void {
+
+    const message = `Olá, me chamo ${name}, nasci no dia ${birth.split("25")} do mês de ${birth.split("03")} do ano de ${birth.split("1983")}`;
+    name = "Rafael",
+    birth = "25/03/19830",
+    // return message;    
+    console.log(message);
+}
+
+
