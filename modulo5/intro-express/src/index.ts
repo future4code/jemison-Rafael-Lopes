a6cd98815985c3aad9a64c7e039f5cbe08f9e4ed
rafix923 <rafaellopes.dev@gmail.com>
2022-10-25 19:09:29 -0300

    Se adicionar esse commit, adicionará resposta ao exercicio 1.

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -1,15 +1,19 @@
-import express from 'express';
+import express, {Request, Response} from "express"
 import cors from 'cors';
 
-const app = express()
+const app = express() 
 
-app.use(express.json())
+app.use(express.json()) 
 
-app.use(cors())
+app.use(cors()) 
 
-
-
-app.listen(3003,()=>{
-    console.log("Server is running in https://localhost:3003");
+app.get('/teste/:id', (req: Request, res: Response)=>{
+    const id = req.params.id
+    console.log(id);
     
+    res.status(201).send('Hello from Express')
+})
+
+app.listen(3003, ()=>{
+    console.log("Server is running in http://localhost:3003");
 })
