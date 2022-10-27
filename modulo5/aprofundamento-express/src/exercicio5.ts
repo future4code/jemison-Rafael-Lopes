2db6b3d7240bee66e4736e9185d15721438062cf
rafix923 <rafaellopes.dev@gmail.com>
2022-10-27 15:34:43 -0300

    Se adicionar esse commit, configurará o expresse e adicionará enunciado  para o exercício 5.

---
+++ modulo5/aprofundamento-express/src/exercicio5.ts
@@ -0,0 +1,16 @@
+// Construa um endpoint de criação de afazer. A resposta deve retornar o array de afazeres atualizado.
+
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+
+
+app.listen(3003,()=>{
+    console.log("Servidor rodando na porta 3003");
+})
