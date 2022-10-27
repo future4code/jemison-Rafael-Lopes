7b71ad27d8b66a393ac58399714f79c856fa80db
rafix923 <rafaellopes.dev@gmail.com>
2022-10-27 14:11:30 -0300

    Se adicionar esse commit, adicionará resposta para o exercício 3.

--- modulo5/aprofundamento-express/src/exercicio2.ts
+++ modulo5/aprofundamento-express/src/exercicio2.ts
@@ -1,7 +1,7 @@
 // Acesse a API do JSONPlaceholder e observe os endpoints que buscam afazeres (”to dos”). 
 // Crie uma variável de tipo para representar cada afazer.
 
-type Chores = {
+export type Chores = {
     "userId": number,
     "id": number,
     "title": string,
