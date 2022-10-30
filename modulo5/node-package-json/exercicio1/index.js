96e446b98cad5dc2c52d5bb750c96d70f4507dfd
rafix923 <rafaellopes.dev@gmail.com>
2022-10-30 17:23:09 -0300

    Se adicionar esse commit, irá refatorar o exercício 1 passando informações com o process.argv.

--- modulo5/node-package-json/exercicio1/index.js
+++ modulo5/node-package-json/exercicio1/index.js
@@ -4,18 +4,24 @@
 // b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
 // "Olá, (Nome)! Você tem (sua idade) anos."
 
-const myName = "Rafael";
-const myAge = 39;
-const statement = `Olá, ${myName}! Você tem ${myAge} anos.`;
+// const myName = "Rafael";
+// const myAge = 39;
+// const statement = `Olá, ${myName}! Você tem ${myAge} anos.`;
 
 function message() {
-    const result = statement;
-    return result
+    const name = process.argv[2];
+    const age = process.argv[3];
+
+    console.log(`Olá, ${name}! Você tem ${age} anos!`);
 }
-const result = message()
-console.log(result);
+message()
 
 // c) Altere o programa acima para que mostre também a sua idade daqui a sete anos.
-const nextSevenYears = Number(myAge) + 7;
-const statement2 = `Olá, ${myName}! Você tem ${myAge} anos. Em sete anos você terá ${nextSevenYears} anos de idade.`
-console.log(statement2);
+
+function statement() {
+    const name1 = process.argv[2];
+    const age1 = Number(process.argv[3])
+
+    console.log(`Olá, ${name1}! Você tem ${age1} anos. Em sete anos você terá ${age1 + 7} anos de idade.`);
+}
+statement()
