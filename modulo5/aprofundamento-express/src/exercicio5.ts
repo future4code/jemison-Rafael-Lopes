e341a1867639bb1367f5e42dc99cc10d68ab998b
rafix923 <rafaellopes.dev@gmail.com>
2022-10-30 10:59:51 -0300

    Se adicionar esse commit, criará a estrutura para usar o express na resolução do exercício 5.

--- modulo5/aprofundamento-express/src/exercicio5.ts
+++ modulo5/aprofundamento-express/src/exercicio5.ts
@@ -1,4 +1,4 @@
-// Construa um endpoint de criação de afazer. A resposta deve retornar o array de afazeres atualizado.
+// Construa um endpoint de criação de afazeres. A resposta deve retornar o array de afazeres atualizado.
 
 import express, {Request, Response} from 'express';
 import cors from 'cors';
@@ -8,6 +8,13 @@ const app = express()
 app.use(express.json())
 
 app.use(cors())
+
+app.post('/chores',(req:Request, res:Response)=>{
+
+
+
+    res.status(200).send('Alguma coisa')
+})
 
 
 
