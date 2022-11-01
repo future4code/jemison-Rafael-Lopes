17fa2985ee2482b2dfbd56c9786bc502cecf38a2
rafix923 <rafaellopes.dev@gmail.com>
2022-11-01 17:27:19 -0300

    Se adicionar esse commit, criará o arquivo type.ts e exportará o tipo produto e incluirá a tipagem do array para o exercício 3.

--- modulo5/fluxo-de-dados-backend/src/data.ts
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -2,30 +2,32 @@
 // Crie um arquivo chamado data.ts que exporta um array de produtos. 
 // Cada produto será representado por um objeto com propriedades: id (string), name (string) e price (number). 
 // Popule manualmente o array com pelo menos 3 produtos. 
+import * as allTypes from './type'
 
-export const products = [
+
+export const products: allTypes.Product[] = [
     {
-        id:1,
+        id: 1,
         name: 'shoes',
         price: '$ 50,00'
     },
     {
-        id:2,
+        id: 2,
         name: 'boots',
         price: '$ 75,00'
     },
     {
-        id:3,
+        id: 3,
         name: 'T-shirt',
         price: '$ 20,00'
     },
     {
-        id:4,
+        id: 4,
         name: 'socks',
         price: '$ 3,00'
     },
     {
-        id:5,
+        id: 5,
         name: 'jacket',
         price: '$ 45,00'
     },
---
+++ modulo5/fluxo-de-dados-backend/src/type.ts
@@ -0,0 +1,9 @@
+// Exercício 3
+// Crie um arquivo chamado type.ts que exporta o tipo produto. Agora vá no arquivo data.ts 
+// e faça a tipagem do array de produtos.
+
+export type Product = {
+    id: number,
+    name: string,
+    price: string
+}
