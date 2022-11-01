220b9f2c35ca2f138f618d9e3af8f632adb5aee8
rafix923 <rafaellopes.dev@gmail.com>
2022-11-01 17:06:23 -0300

    Se adicionar esse commit, incluirá resposta para o exercício 1.

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,9 @@
+// Exercício 2
+// Crie um arquivo chamado data.ts que exporta um array de produtos. 
+// Cada produto será representado por um objeto com propriedades: id (string), name (string) e price (number). 
+// Popule manualmente o array com pelo menos 3 produtos. 
+
+
+export const products = [
+
+]
--- modulo5/fluxo-de-dados-backend/src/index.ts
+++ modulo5/fluxo-de-dados-backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors'
 
 const app = express();
@@ -6,12 +6,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Exercício 1
+// Crie uma nova API do zero e desenvolva um endpoint de teste com método GET 
+// no path “/test” que retorna a mensagem: “Servidor rodando na porta (colocar porta)!”. 
+
+app.get('/test', (req: Request, res: Response) => {
+    res.status(200).send("Servidor rodando na porta 3003")
+})
+
+// Exercício 2 no arquivo externo data.ts
 
 
 
 
-
-app.listen(3003, ()=>{
+// Servidor
+app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");
-    
+
 })
