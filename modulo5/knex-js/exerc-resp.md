68ba7add88a63bc4b4dd29477fe4534ad0d46f3e
rafix923 <rafaellopes.dev@gmail.com>
2022-11-18 19:57:38 -0300

    Se adicionar esse commit, irá adicionar respostas aos itens a e b do exercício 1

---
+++ modulo5/knex-js/exerc-resp.md
@@ -0,0 +1,9 @@
+# Respostas para os exercícios
+
+a) Explique como é a resposta que temos quando usamos o raw.
+
+R: Retornou os parâmetros de um ator específico: id, name, salary, birth_date e gender.
+
+b) Faça uma função que busque um ator pelo nome.
+
+R: 
--- modulo5/knex-js/src/index.ts
+++ modulo5/knex-js/src/index.ts
@@ -1,6 +1,7 @@
-import express from "express"
-
-import cors from 'cors'
+import express from "express";
+import cors from 'cors';
+import { Request, Response } from "express";
+import connection from "./database/connection";
 
 const app = express()
 
@@ -9,7 +10,61 @@ app.use(express.json())
 app.use(cors())
 
 
+// const getActorById = async (id: string): Promise<any> => {
+//     const result = await connection.raw(`
+//     SELECT * FROM Actor WHERE id = '${id}'
+//   `)
+//     return result[0][0]
+// }
 
+// Assim a chamada funciona fora dos endpoints com .then()/.catch
+
+// getActorById("001")
+// 	.then(result => {
+// 		console.log(result)
+// 	})
+// 	.catch(err => {
+// 		console.log(err)
+// 	});
+
+// Assim a chamada funciona fora dos endpoints com await
+
+// (async () => {
+//     console.log(await getActorById("001"))
+// })()
+
+// Ou então podemos chamá-la dentro de um endpoint
+
+// app.get("/users/:id", async (req: Request, res: Response) => {
+//     try {
+//         const id = req.params.id
+
+//         await getActorById(id)
+
+//         res.end()
+//     } catch (error: any) {
+//         console.log(error.message)
+//         res.status(500).send("Unexpected error")
+//     }
+// })
+
+
+// Resposta Exercício 1- b
+
+const searchActor = async (name: string): Promise<any> => {
+    const result = await connection.raw(`
+      SELECT * FROM Actor WHERE name = "${name}"
+    `)
+    return result
+  }
+
+  searchActor("Moacyr Franco")
+	.then(result => {
+		console.log(result)
+	})
+	.catch(err => {
+		console.log(err)
+	});
 
 // Configuração do servidor
 
