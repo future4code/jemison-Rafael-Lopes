ed97157c4d4310ce1cb65de28ec186d143cdf4fc
rafix923 <rafaellopes.dev@gmail.com>
2022-12-07 13:18:04 -0300

    fit: Se adicionar esse commit, cria o arquivo Product.ts com as propriedades e o construtor

---
+++ modulo5/classes-encapsulamento/src/class/Product.ts
@@ -0,0 +1,8 @@
+export class Product{
+    constructor(private id:string, private name:string, private price:number){
+        this.id = id;
+        this.name=name;
+        this.price=price
+    }
+    
+}
