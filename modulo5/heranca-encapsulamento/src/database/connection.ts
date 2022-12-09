1a58f4ab62fa6149a8768c24305126cb090ac46d
rafix923 <rafaellopes.dev@gmail.com>
2022-12-08 22:42:13 -0300

    feat:Se adicionar esse commit, criará e adicionará configuração do arquivo BaseDataBase.ts

--- modulo5/heranca-encapsulamento/src/database/connection.ts
+++ modulo5/heranca-encapsulamento/src/database/connection.ts
@@ -7,7 +7,7 @@ const connection = knex({
    client: "mysql",
    connection: {
       host: process.env.DB_HOST,
-      port: 3303,
+      port: 3003,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
