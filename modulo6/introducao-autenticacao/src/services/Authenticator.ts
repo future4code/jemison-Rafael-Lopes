37178fade60bb59d9ed84a92c73e9c9363e4f7ec
rafix923 <rafaellopes.dev@gmail.com>
2023-02-01 22:36:47 -0300

    chore(Config): Copiando diretórios como base para os exercícios de fixação de introdução a autenticação

---
+++ modulo6/introducao-autenticacao/src/services/Authenticator.ts
@@ -0,0 +1,25 @@
+import { AuthenticationData } from "../model/user";
+import * as jwt from "jsonwebtoken";
+import { Unauthorized } from "../error/customError";
+
+
+
+export class Authenticator {
+    public generateToken = ({ id }: AuthenticationData): string => {
+        const token = jwt.sign(
+            { id },
+            process.env.JWT_KEY as string,
+            { expiresIn: "1h" }
+        )
+        return token
+    };
+
+    getTokenData = (token: string): AuthenticationData => {
+        try {
+            const payload = jwt.verify(token, process.env.JWT_KEY as string) as AuthenticationData
+            return payload
+        } catch (error: any) {
+            throw new Unauthorized
+        }
+    }
+};
