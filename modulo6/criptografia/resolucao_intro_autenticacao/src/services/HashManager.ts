ad96455bb5e231aa26734bb53a4ae3f61a8738ba
rafix923 <rafaellopes.dev@gmail.com>
2023-02-02 17:53:09 -0300

    feat(compare): Cria a função que verifica se uma string é correspondente a um hash, usa a função compare do bcryptjs

--- modulo6/criptografia/resolucao_intro_autenticacao/src/services/HashManager.ts
+++ modulo6/criptografia/resolucao_intro_autenticacao/src/services/HashManager.ts
@@ -1,12 +1,17 @@
 import * as bcrypt from "bcryptjs"
 
 export class HashManager {
-    
-// b) Instale o bcryptjs no seu projeto e comece criando a função generateHash(), que será responsável por criptografar uma string usando o bcryptjs.   
+
+    // b) Cria a função generateHash(), que será responsável por criptografar uma string usando o bcryptjs.   
     generateHash = async (s: string): Promise<string> => {
         const rounds: number = Number(process.env.BCRYPT_COST);
         const salt = await bcrypt.genSalt(rounds);
         const result = await bcrypt.hash(s, salt);
+        return result
+    }
+    // c) Cria a função que verifica se uma string é correspondente a um hash, use a função compare do bcryptjs
+    compareHash = async (s: string, hashText: string): Promise<boolean> => {
+        const result = await bcrypt.compare(s, hashText)
         return result
     }
 }
