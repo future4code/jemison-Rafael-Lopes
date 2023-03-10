94a0e58b4d0b2a837aa8534045cbeec2f40bb57f
rafix923 <rafaellopes.dev@gmail.com>
2023-03-10 17:44:48 -0300

    refactore(user): Modifica o type User para que ele se torne uma classe.

--- modulo6/classes-encapsulamento/src/endpoints/createUser.ts
+++ modulo6/classes-encapsulamento/src/endpoints/createUser.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
+import { User } from "../class/user"
 import connection from "../database/connection"
 import { TABLE_USERS } from "../database/tableNames"
-import { User } from "../models/User"
 
 export const createUser = async (req: Request, res: Response) => {
     let errorCode = 400
@@ -13,18 +13,22 @@ export const createUser = async (req: Request, res: Response) => {
             throw new Error("Body inválido.")
         }
 
-        const newUser: User = {
-            id: Date.now().toString(),
-            email,
-            password
-        }
+        // const newUser: User = {
+        //     id: Date.now().toString(),
+        //     email,
+        //     password
+        // }
 
-        await connection(TABLE_USERS).insert({
-            id: newUser.id,
-            email: newUser.email,
-            password: newUser.password
-        })
-        
+        // await connection(TABLE_USERS).insert({
+        //     id: newUser.id,
+        //     email: newUser.email,
+        //     password: newUser.password
+        // })
+
+        const id = Date.now().toString()
+        const newUser = new User(id, email, password)
+
+
         res.status(201).send({ message: "Usuário criado", user: newUser })
     } catch (error) {
         res.status(errorCode).send({ message: error.message })
