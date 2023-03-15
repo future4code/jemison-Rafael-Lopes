17235777db2d49e2e4e779a1bd1b6f84f3aa7264
Rafael Lopes <rafaellopes.dev@gmail.com>
2023-03-14 22:14:08 -0300

    Delete Product.ts

--- modulo5/classes-encapsulamento/src/class/Product.ts
+++
@@ -1,8 +0,0 @@
-export class Product{
-    constructor(private id:string, private name:string, private price:number){
-        this.id = id;
-        this.name=name;
-        this.price=price
-    }
-    
-}
